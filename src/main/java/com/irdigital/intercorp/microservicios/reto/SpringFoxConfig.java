06b0d36ffe1e290fc6594966f4bb6a4b2d1743f7
Eli Tanta Melgar <eatanta@indracompany.com>
2019-10-02 04:17:00 -0500

    elitanta
    mofigicacion del swagger

--- src/main/java/com/irdigital/intercorp/microservicios/reto/SpringFoxConfig.java
+++ src/main/java/com/irdigital/intercorp/microservicios/reto/SpringFoxConfig.java
@@ -17,7 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
-    public static final Contact DEFAULT_CONTACT = new Contact("Eli Tanta", "https://github.com/allconsulting-peru", "eliantonio6@gmail.com");
+    public static final Contact DEFAULT_CONTACT = new Contact("Eli Tanta", "https://github.com/elitantamelgar", "eliantonio6@gmail.com");
     public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("API_CLIENTE", "Reto intercorp", "1.0", "urn:tos",
             DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
 
